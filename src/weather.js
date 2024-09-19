0aee60c108bf2ac99d00a06a90a6c9f3d12924cb
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-09-19 16:36:44 -0700

    completed videos 2 and 3

--- src/weather.js
+++ src/weather.js
@@ -2,11 +2,30 @@ import { useState } from "react";
 import './Weather.css';
 
 function Weather() {
+  const [zip, setZip] = useState('94210')
+  const [unit, setUnit] = useState(' ')
+
   return (
     <div className="Weather">
+
+      <h1> {zip} {unit} </h1>
+
       <form>
-        <input placeholder="Enter zip code" />
-        <button>Submit</button>
+        <div>
+        <input 
+            placeholder="Enter zip code"
+            value={zip}
+            onChange={e => setZip(e.target.value)}
+            />
+            <button>Submit</button>
+        </div>
+        
+        <select onChange={e => setUnit(e.target.value)}>
+        <option value="metric">Celcius</option>
+        <option value="imperial">Fahrenheit</option>
+        <option value="standard">Kelvin</option>
+        </select>
+
       </form>
     </div>
   );
