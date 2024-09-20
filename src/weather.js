10e10aa06f06a7abba1ded8d66b8d9ef5408abf4
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-09-19 17:27:03 -0700

    completed lessons 4-6

--- src/weather.js
+++ src/weather.js
@@ -1,31 +1,66 @@
 import { useState } from "react";
 import './Weather.css';
 
+import RadioButton from'./RadioButton'
+
 function Weather() {
-  const [zip, setZip] = useState('94210')
+  const [zip, setZip] = useState(' ')
   const [unit, setUnit] = useState(' ')
+  const [data, setData] = useState(null)
 
   return (
     <div className="Weather">
+      {data && <h1> {data.temp} </h1>}
+      
 
-      <h1> {zip} {unit} </h1>
 
-      <form>
+      <form onSubmit={ e => {
+        e.preventDefault()
+        // load weather data
+        // setData( newData )
+      }}>
         <div>
-        <input 
+          <input 
             placeholder="Enter zip code"
             value={zip}
             onChange={e => setZip(e.target.value)}
             />
-            <button>Submit</button>
+
+
+            <button type="submit">Submit</button>
         </div>
-        
-        <select onChange={e => setUnit(e.target.value)}>
+
+        <select 
+        value={unit}
+        onChange={e => setUnit(e.target.value)}
+        >
         <option value="metric">Celcius</option>
         <option value="imperial">Fahrenheit</option>
         <option value="standard">Kelvin</option>
         </select>
 
+
+        <RadioButton 
+          label = "metric"
+          name = "unit"
+          checked = {unit === "metric"}
+          onChange = {() => setUnit('metric')} 
+        />
+        
+        <RadioButton 
+          label = "imperial"
+          name = "unit"
+          checked = {unit === "imperial"}
+          onChange = {() => setUnit('imperial')} 
+        />
+        
+        <RadioButton 
+          label = "standard"
+          name = "unit"
+          checked = {unit === "standard"}
+          onChange = {() => setUnit('standard')} 
+        />
+        
       </form>
     </div>
   );
